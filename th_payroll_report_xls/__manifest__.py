582e878c3db52a32f48489dccbab1427fd21eded
thomas51516 <thomas51516@gmail.com>
2020-04-03 07:01:33 +0000

    Add files via upload

---
+++ th_payroll_report_xls/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Rapport de paie en excel',
+    'version': '1.0',
+    'licence': 'LGPL-3',
+    'category': 'payroll',
+    'sequence': 60,
+    'summary': 'Générer le rapport au format xlsx',
+    'description': "It shows payroll report in excel for given month",
+    'author':'Roots-Technologies',
+    'depends': ['base','hr', 'hr_payroll'],
+    'data': ['wizard/payroll_report_wiz.xml'
+      ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
